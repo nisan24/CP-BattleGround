a8a84749f456388a013b6d05dd90541b899d4654
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-03-23 17:57:19 +0600

    cp update

---
+++ Codeforces-Solutions/CF_Random_Solutions/A_Young_Physicist.cpp
@@ -0,0 +1,45 @@
+/**
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name : 
+ *  Author       : Nisan Hossain
+ *  Created Date : 2025-02-25
+ * -------------------------------------------------
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+// ---------- Solve Function ----------
+void solve()
+{
+    
+
+}
+
+// ---------- Main Function ----------
+int main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
