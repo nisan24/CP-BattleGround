f35228303ceec349e476bb777281637ad4930eeb
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-01 22:13:06 +0600

    update

---
+++ Codeforces-Solutions/Assit sheets/Sheet #1 (Data type - conditions)/Y_The_last_2_digits.cpp
@@ -0,0 +1,58 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-01 20:30:36
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    ll a, b, c, d;
+    cin >> a >> b >> c >> d;
+    a = a % 100;
+    b = b % 100;
+    c = c % 100;
+    d = d % 100;
+    // ll ans = (a * b * c * d) % 10;
+    // cout << ans << nl;
+    ll ans2 = (a * b * c * d) % 100;
+    cout << ans2 << nl;
+     
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
