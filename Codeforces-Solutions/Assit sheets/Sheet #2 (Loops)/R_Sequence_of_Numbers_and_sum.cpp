7c1bf0dca028552433516cfaa1093b9eab9f7ea1
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-08 20:20:22 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #2 (Loops)/R_Sequence_of_Numbers_and_sum.cpp
@@ -0,0 +1,68 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/R
+ *  Problem Name : R. Sequence of Numbers and Sum
+ *  Problem Type : Implementation
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-08 19:25:18
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve(int n, int m)
+{
+    int sum = 0;
+    int start = min(n, m);
+    int end = max(n, m);
+
+    for (int i = start; i <= end; ++i)
+    {
+        cout << i << " ";
+        sum += i;
+    }
+    cout << "sum =" << sum << endl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    int n, m;
+
+    while (cin >> n >> m && min(n, m) > 0)
+    {
+        solve(n, m);
+    }
+
+    return 0;
+}
+
+
+// Accepted
+
+// ---------- End of Code ----------
