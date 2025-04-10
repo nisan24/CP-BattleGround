2903a3892f8449aba5685ab092418eb002a6bb86
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-10 14:35:38 +0600

    1 problme added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #2 (Loops)/T_Shape2.cpp
@@ -0,0 +1,58 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/T
+ *  Problem Name : T. Shape 2
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-10 14:25:26
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << string(n - i, ' ') << string(2 * i - 1, '*') << nl;
+        // cout << string(2 * i - 1, '*') << nl;
+    }
+   
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
