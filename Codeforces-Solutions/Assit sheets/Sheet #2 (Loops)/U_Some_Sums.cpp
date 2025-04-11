686167a84a9581d56422d7b96809778a95032fbd
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-11 21:07:50 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #2 (Loops)/U_Some_Sums.cpp
@@ -0,0 +1,76 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-11 20:49:03
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+int digitSum(int n)
+{
+    int sum = 0;
+    while (n > 0)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int N, A, B;
+    cin >> N >> A >> B;
+
+    int total = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        int dSum = digitSum(i);
+        // cout << "Dsum- "<< dSum << " ";
+        if (dSum >= A && dSum <= B)
+        {
+            total += i;
+            // cout << "i - "<< i << " ";
+        }
+        // nnl;
+    }
+    // nnl;
+
+    cout << total << nl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
