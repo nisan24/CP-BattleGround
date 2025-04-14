3d232faf3d6beec2d37bc62bac7a1802671baf4e
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-14 22:32:08 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #2 (Loops)/Z_Three_Numbers.cpp
@@ -0,0 +1,68 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/Z
+ *  Problem Name : Three Numbers
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-14 22:19:51
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int start, end;
+    cin >> start >> end;
+
+    int count = 0;
+
+    for (int i = 0; i <= start; i++)
+    {
+        for (int j = 0; j <= start; j++)
+        {
+            int k = end - i - j;
+
+            if (k >= 0 && k <= start)
+            {
+                count++;
+            }
+        }
+    }
+
+    cout << count << nl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
