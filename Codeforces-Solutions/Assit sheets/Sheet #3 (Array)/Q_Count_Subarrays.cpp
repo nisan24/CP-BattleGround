25b750b917d886eca864d63f65b5400804ac1811
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-19 22:21:00 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #3 (Array)/Q_Count_Subarrays.cpp
@@ -0,0 +1,82 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/Q
+ *  Problem Name : Q. Count Subarrays
+ *  Problem Type : Arrays
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-19 22:07:40
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        ll result = n;
+
+        int length = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] >= arr[i - 1])
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+            }
+            result += (length - 1);
+        }
+
+        cout << result << endl;
+    
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    ll TC;
+    cin >> TC;
+    while (TC--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Accepted
