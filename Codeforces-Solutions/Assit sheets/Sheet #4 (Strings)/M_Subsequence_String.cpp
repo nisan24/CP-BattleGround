eaf19f3e6d75c95e85203fcfa4e3357a1b15da4f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-04-29 23:29:56 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #4 (Strings)/M_Subsequence_String.cpp
@@ -0,0 +1,58 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/M
+ *  Problem Name : M. Subsequence String
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-04-29 23:24:47
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    string s, t = "hello";
+    cin >> s;
+
+    int j = 0;
+    for (char c : s)
+        if (c == t[j])
+            j++;
+
+    cout << (j == t.size() ? "YES" : "NO") << '\n';
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
