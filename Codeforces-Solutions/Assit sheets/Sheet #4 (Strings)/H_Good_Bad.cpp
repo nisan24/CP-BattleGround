b592f6cd5dbd3c706e6203269c03e1352aabdef7
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-05-05 20:53:29 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #4 (Strings)/H_Good_Bad.cpp
@@ -0,0 +1,65 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/H
+ *  Problem Name : Good Bad
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-05-05 20:43:48
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    string x;
+    cin >> x;
+
+    if (x.find("101") != string::npos || x.find("010") != string::npos)
+    {
+        cout << "Good\n";
+    }
+    else
+    {
+        cout << "Bad\n";
+    }
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    ll TC;
+    cin >> TC;
+    while (TC--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// Accepted
