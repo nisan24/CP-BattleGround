c36ab0456405303c8ca630834e5b56355e4a6513
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-05-12 20:40:21 +0600

    1 problem solve

---
+++ Codeforces-Solutions/Assit sheets/Sheet #5(Functions)/E_Swap.cpp
@@ -0,0 +1,55 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/E
+ *  Problem Name : E. Swap
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-05-12 20:36:51
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve(int x, int y)
+{
+    cout << y << " " << x << nl;
+
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    int x, y;
+    cin >> x >> y;
+
+    solve(x, y);
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
