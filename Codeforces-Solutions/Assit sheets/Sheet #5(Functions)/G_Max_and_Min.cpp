7f3e287cdcb44e244c19f7aaecec0d1b985fa17f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-05-13 21:34:53 +0600

    1 problem solved

---
+++ Codeforces-Solutions/Assit sheets/Sheet #5(Functions)/G_Max_and_Min.cpp
@@ -0,0 +1,61 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/G
+ *  Problem Name : G. Max and Min
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-05-13 21:25:55
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int n;
+    cin >> n;
+    vi ar(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> ar[i];
+    }
+    srt(ar);
+    int mx = ar[n - 1];
+    int mn = ar[0];
+    cout << mn << " " << mx << nl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
