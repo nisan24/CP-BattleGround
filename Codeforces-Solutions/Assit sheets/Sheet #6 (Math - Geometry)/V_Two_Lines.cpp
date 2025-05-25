b01c5925943b2d552705052779b982beeabedb94
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-05-25 21:06:53 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #6 (Math - Geometry)/V_Two_Lines.cpp
@@ -0,0 +1,64 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223338/problem/V
+ *  Problem Name : Two Lines
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-05-25 21:02:52
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    double x1, y1, x2, y2;
+    double x3, y3, x4, y4;
+
+    cin >> x1 >> y1 >> x2 >> y2;
+    cin >> x3 >> y3 >> x4 >> y4;
+
+    ll lhs = (y2 - y1) * (x4 - x3);
+    ll rhs = (y4 - y3) * (x2 - x1);
+
+    if (lhs == rhs)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
