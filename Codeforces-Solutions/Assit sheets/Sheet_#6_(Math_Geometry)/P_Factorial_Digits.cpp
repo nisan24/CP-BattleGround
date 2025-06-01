d9e23a426ef4e6de410995fca79531dc184a56e7
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-06-01 18:10:39 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet_#6_(Math_Geometry)/P_Factorial_Digits.cpp
@@ -0,0 +1,66 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223338/problem/P
+ *  Problem Name : P. Factorial Digits
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-06-01 18:05:02
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int n;
+    cin >> n;
+
+    if (n == 0 || n == 1)
+    {
+        cout << "Number of digits of " << n << "! is 1" << noboolalpha;
+        return;
+    }
+
+    double logSum = 0;
+    for (int i = 2; i <= n; ++i)
+        logSum += log10(i);
+
+    int digits = static_cast<int>(floor(logSum)) + 1;
+    cout << "Number of digits of " << n << "! is " << digits << nl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
