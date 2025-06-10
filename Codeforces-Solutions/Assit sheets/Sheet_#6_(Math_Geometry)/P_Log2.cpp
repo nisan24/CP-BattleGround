2ffa298d34d40bd36d836e797160350ca7d0e28b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-06-10 23:17:24 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet_#6_(Math_Geometry)/P_Log2.cpp
@@ -0,0 +1,66 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/P
+ *  Problem Name : Log2
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-06-10 23:12:05
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+ll log2Recursive(ll n)
+{
+    if (n == 1)
+        return 0;
+
+    return 1 + log2Recursive(n / 2);
+}
+
+void solve()
+{
+    ll n;
+    cin >> n;
+
+    ll result = log2Recursive(n);
+
+    cout << result << nl;
+
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
