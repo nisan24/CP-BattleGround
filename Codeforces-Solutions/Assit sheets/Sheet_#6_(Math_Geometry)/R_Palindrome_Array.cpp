a446e4f1a8329e3ccff7bbeaf7c1759ba394fbbd
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-06-12 21:34:25 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet_#6_(Math_Geometry)/R_Palindrome_Array.cpp
@@ -0,0 +1,77 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223339/problem/R
+ *  Problem Name : R. Palindrome Array
+ *  Category     : Math, Geometry
+ *  Tags         : Array, Recursion
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-06-12 21:26:34
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+bool isPalindrome(int arr[], int left, int right)
+{
+    if (left >= right)
+        return true;
+
+    if (arr[left] != arr[right])
+        return false;
+
+    return isPalindrome(arr, left + 1, right - 1);
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    if (isPalindrome(arr, 0, n - 1))
+        yes;
+    else
+        no;
+
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+
+// Accepted
+// ---------- End of Code ----------
