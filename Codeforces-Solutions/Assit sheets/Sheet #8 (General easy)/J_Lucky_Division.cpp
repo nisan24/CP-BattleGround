9a888c829fa66c28b2b56e9b9ab0964ad75b589b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-06-25 20:46:25 +0600

    1 problem added

---
+++ Codeforces-Solutions/Assit sheets/Sheet #8 (General easy)/J_Lucky_Division.cpp
@@ -0,0 +1,72 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223206/problem/J
+ *  Problem Name : J. Lucky Division (Almost Lucky Number)
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-06-25 20:37:21
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+bool isLucky(int num)
+{
+    while (num > 0)
+    {
+        int digit = num % 10;
+        if (digit != 4 && digit != 7)
+            return false;
+        num /= 10;
+    }
+    return true;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (isLucky(i) && n % i == 0)
+        {
+            yes;
+            return;
+        }
+    }
+    no;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
