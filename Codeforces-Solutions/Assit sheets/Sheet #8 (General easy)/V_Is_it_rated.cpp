480e62762e94fdd2a962dd71fe01331995491507
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-07-06 22:48:45 +0600

    1 problem  solved

---
+++ Codeforces-Solutions/Assit sheets/Sheet #8 (General easy)/V_Is_it_rated.cpp
@@ -0,0 +1,80 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223206/problem/V
+ *  Problem Name : Is it rated?
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-07-06 22:40:06
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int n;
+    cin >> n;
+
+    bool unrated = false;
+    int prevRating = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+
+        if (a != b)
+        {
+            cout << "rated" << nl;
+            return;
+        }
+
+        if (prevRating != -1 && a > prevRating)
+        {
+            unrated = true;
+        }
+
+        prevRating = a;
+    }
+
+    if (unrated)
+        cout << "unrated" << nl;
+    else
+        cout << "maybe" << nl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
+
+// Accepted
+// ---------- End of Code ----------
