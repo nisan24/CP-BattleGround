7a585b49ba4004bf3010c7c02cc299ae6d6f6319
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-07-07 22:49:57 +0600

    update

--- Codeforces-Solutions/Assit sheets/Sheet #8 (General easy)/Q_Night_at_the_Museum.cpp
+++ Codeforces-Solutions/Assit sheets/Sheet #8 (General easy)/Q_Night_at_the_Museum.cpp
@@ -32,6 +32,7 @@ using namespace std;
 
 int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
 
+
 // ---------- Solve Function ----------
 void solve()
 {
