048bffba06273ae74acefc778372680b1ecd8c9f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-02 21:44:10 +0600

    1 problem solved

---
+++ Codeforces-Solutions/Assit sheets/Sheet #9 (General medium)/Y_Polo_the_Penguin_and_Segments.cpp
@@ -0,0 +1,63 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-08-02 21:40:39
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vpi segments(n);
+    int total = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        total += (r - l + 1);
+    }
+
+    int extra = (k - (total % k)) % k;
+    cout << extra << nl;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    solve();
+
+    return 0;
+}
