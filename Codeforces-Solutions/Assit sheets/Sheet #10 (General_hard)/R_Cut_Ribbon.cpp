8f55565cfe7ef94802b1f8475ffb66a58907df44
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-05 22:54:49 +0600

    Updated

--- Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/R_Cut_Ribbon.cpp
+++ Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/R_Cut_Ribbon.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
- *  Problem Name :  
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223340/problem/R
+ *  Problem Name :
  *  Author       : Nisan Hossain
  *	Created At   : 2025-08-05 22:52:06
  *  Language     : C++
