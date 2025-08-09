34a9a5a6f71baa40763854500a93c4fba3930390
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-09 23:32:51 +0600

    1 problem solved

---
+++ Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/Q_Bulbs.cpp
@@ -0,0 +1,66 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ *	Created At   : 2025-08-09 23:22:14
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<ll>
+#define pi pair<ll, ll>
+#define vpi vector<pi>
+#define nl "\n"
+#define nnl cout << "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define setPrec(x) fixed << setprecision(x)
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define mset(ar, option) memset(ar, option, sizeof(ar))
+#define allsum(v) accumulate(v.begin(), v.end(), 0LL)
+#define fast_io                  \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL); 			  \
+    cout.tie(NULL)
+
+int isprime(int n){if(n==1) return 0;for(int i = 2; i <= sqrt(n); i++){if(n % i == 0)return 0;}return 1;}
+
+// ---------- Solve Function ----------
+
+int countMoments(const vector<int> &vec)
+{
+    int n = vec.size();
+    int moments = 0, maxBulb = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        maxBulb = max(maxBulb, vec[i]);
+        if (maxBulb == i + 1)
+            moments++;
+    }
+    return moments;
+}
+
+// ---------- Main Function ----------
+int32_t main(int argc, char const *argv[])
+{
+    fast_io;
+
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> vec[i];
+
+    cout << countMoments(vec) << nl;
+
+    return 0;
+}
