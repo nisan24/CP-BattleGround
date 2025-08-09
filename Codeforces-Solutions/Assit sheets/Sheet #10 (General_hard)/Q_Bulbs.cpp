6331eb6b1fdaf9ee5df74b8901677eb610f7b156
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-09 23:33:58 +0600

    Updated

--- Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/Q_Bulbs.cpp
+++ Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/Q_Bulbs.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
- *  Problem Name :  
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223340/problem/Q
+ *  Problem Name :
  *  Author       : Nisan Hossain
  *	Created At   : 2025-08-09 23:22:14
  *  Language     : C++
