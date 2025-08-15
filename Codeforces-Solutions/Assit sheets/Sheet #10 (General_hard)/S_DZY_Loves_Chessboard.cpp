5213263199efa946ccf0371fa765636f14bf87d5
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-15 22:12:47 +0600

    Updated

--- Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/S_DZY_Loves_Chessboard.cpp
+++ Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/S_DZY_Loves_Chessboard.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
- *  Problem Name :  
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223340/problem/S
+ *  Problem Name :
  *  Author       : Nisan Hossain
  *	Created At   : 2025-08-15 22:03:47
  *  Language     : C++
