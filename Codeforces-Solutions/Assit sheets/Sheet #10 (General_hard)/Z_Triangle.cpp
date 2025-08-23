c385a46d03d42a57330decd76bd96c0dd5674f59
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-23 22:40:47 +0600

    Updated

--- Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/Z_Triangle.cpp
+++ Codeforces-Solutions/Assit sheets/Sheet #10 (General_hard)/Z_Triangle.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
- *  Problem Name :  
+ *  Problem Link : https://codeforces.com/group/MWSDmqGsZm/contest/223340/problem/Z
+ *  Problem Name :
  *  Author       : Nisan Hossain
  *	Created At   : 2025-08-23 22:35:23
  *  Language     : C++
