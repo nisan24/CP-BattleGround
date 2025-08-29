32ab0e8e39217498bfd280fb2d75bd56a42e1c96
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-08-29 23:12:18 +0600

    Updated

--- LeetCode-Solutions/290_Word_Pattern.cpp
+++ LeetCode-Solutions/290_Word_Pattern.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
- *  Problem Name :  
+ *  Problem Link : https://leetcode.com/problems/word-pattern/description/?envType=problem-list-v2&envId=string
+ *  Problem Name :
  *  Author       : Nisan Hossain
  *	Created At   : 2025-08-29 23:11:18
  *  Language     : C++
