b8f2dad7681e34d769fca60e91ab840e66426690
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-18 23:04:24 +0600

    Updaed

--- LeetCode-Solutions/392_Is_Subsequence.cpp
+++ LeetCode-Solutions/392_Is_Subsequence.cpp
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/is-subsequence/description/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-09-18 23:03:21
