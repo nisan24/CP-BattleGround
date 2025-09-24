e363bd56eac83c2d8fa17a62e6885948172dacf7
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-24 23:28:03 +0600

    1 problem added

--- LeetCode-Solutions/144_Binary_Tree_Preorder_Traversal.cpp
+++ LeetCode-Solutions/144_Binary_Tree_Preorder_Traversal.cpp
@@ -28,5 +28,3 @@ private:
         preorder(node->right, res);
     }
 };
-
-
---
+++ LeetCode-Solutions/78_Subsets.cpp
@@ -0,0 +1,35 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-09-24 23:27:29
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int num = pow(2, n), i = 0;
+        
+        while(i<num)
+        {
+            vector<int> temp;
+            for(int j=0; j<n; j++)
+            {
+                if(i & 1<<j)
+                    temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+            i++;
+        }
+        
+        return ans;
+    }
+};
+
+// Accepted
