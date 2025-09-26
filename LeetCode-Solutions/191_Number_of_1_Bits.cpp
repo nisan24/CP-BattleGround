baea26012419d4b767cd6fa774c3c7d199efc114
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-26 23:16:53 +0600

    1 problem added

---
+++ LeetCode-Solutions/191_Number_of_1_Bits.cpp
@@ -0,0 +1,24 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-09-26 23:16:29
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
+
+// Accepted
