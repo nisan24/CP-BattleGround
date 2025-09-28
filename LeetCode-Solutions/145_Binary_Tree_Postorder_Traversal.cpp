b80235f8f610c58a7ace14dad53839fc1e6fb770
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-28 23:34:24 +0600

    1 problem added

---
+++ LeetCode-Solutions/145_Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,39 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-09-28 23:33:58
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> result;
+    void postorder(TreeNode* root){
+        if (root == NULL) return;
+        postorder(root->left);
+        postorder(root->right);
+        result.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        postorder(root);
+        return result;
+    }
+};
+
+// Accepted
