99786fe2172cba7087c2eee0b77ea95a7a6dd7fb
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-29 23:26:04 +0600

    1 problem added

--- LeetCode-Solutions/386_Lexicographical_Numbers.cpp
+++ LeetCode-Solutions/386_Lexicographical_Numbers.cpp
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/lexicographical-numbers/description/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-09-29 23:25:21
