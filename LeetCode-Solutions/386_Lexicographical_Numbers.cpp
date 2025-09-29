5894344c37ca804a5c690cd57346f4edf8c02232
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-29 23:25:45 +0600

    1 problem added

---
+++ LeetCode-Solutions/386_Lexicographical_Numbers.cpp
@@ -0,0 +1,35 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-09-29 23:25:21
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    void dfs(int curr, int n, vector<int> &result) {
+        if (curr > n) return;
+        result.push_back(curr);
+        for (int i = 0; i <= 9; i++) {
+            int next = curr * 10 + i;
+            if (next > n) return;
+            dfs(next, n, result);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+        for (int i = 1; i <= 9; i++) {
+            if (i > n) break;
+            dfs(i, n, result);
+        }
+
+        return result;
+    }
+};
+
+// Accepted
