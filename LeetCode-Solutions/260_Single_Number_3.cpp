7122687bc6f544f663c095d10089f5a129faaf8f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-09-30 21:09:35 +0600

    1 problem added

---
+++ LeetCode-Solutions/260_Single_Number_3.cpp
@@ -0,0 +1,31 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-09-30 21:09:12
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int> &nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        vector<int> ans;
+        for (auto &[num, f] : freq) {
+            if (f == 1) {
+                ans.push_back(num);
+            }
+        }
+        return ans;
+    }
+
+};
+
+// Accepted
