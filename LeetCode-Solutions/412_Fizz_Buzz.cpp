40be137b9520d4b8439ddd2907877560685ef5bf
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-02 23:02:17 +0600

    1 problem added

---
+++ LeetCode-Solutions/412_Fizz_Buzz.cpp
@@ -0,0 +1,33 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-02 23:01:53
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                ans.push_back("FizzBuzz");
+            } else if (i % 3 == 0) {
+                ans.push_back("Fizz");
+            } else if (i % 5 == 0) {
+                ans.push_back("Buzz");
+            } else {
+                ans.push_back(to_string(i));
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Accepted
