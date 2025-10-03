b8a00a5091856c3469c109f59cfc50290afb9542
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-03 22:55:55 +0600

    1 problem added

--- LeetCode-Solutions/237_Delete_Node_in_a_Linked_List.cpp
+++ LeetCode-Solutions/237_Delete_Node_in_a_Linked_List.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/delete-node-in-a-linked-list/description/
- *  Problem Name :  
+ *  Problem Name : Delete Node in a Linked List
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-03 22:54:46
  *  Language     : C++
