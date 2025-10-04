acba95400d8705940ff4f52e59645ed452cc1f6f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-04 22:40:31 +0600

    1 problem added

---
+++ LeetCode-Solutions/338_Counting_Bits.cpp
@@ -0,0 +1,25 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-04 22:40:11
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1, 0);
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i >> 1] + (i & 1);
+        }
+
+        return dp;
+    }
+};
+
+// Accepted
