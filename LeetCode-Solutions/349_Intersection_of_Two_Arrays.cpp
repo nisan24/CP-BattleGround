cfb22df4cdb4fb0eb90865fbe677c1dcd4bf8f8b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-06 22:38:39 +0600

    1 problem added

---
+++ LeetCode-Solutions/349_Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,29 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-06 22:38:14
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        vector<int> result;
+
+        for (int num : nums2) {
+            if (s1.count(num)) {
+                result.push_back(num);
+                s1.erase(num);
+            }
+        }
+
+        return result;
+    }
+};
+
+// Accepted
