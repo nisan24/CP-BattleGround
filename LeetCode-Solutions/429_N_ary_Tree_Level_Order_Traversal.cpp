cdfaa7dd4322d2d29f68ae42af2bac6aa2fc93b4
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-07 22:23:25 +0600

    updated

---
+++ LeetCode-Solutions/429_N_ary_Tree_Level_Order_Traversal.cpp
@@ -0,0 +1,64 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-07 22:23:08
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        if (!root) return {};
+
+        vector<vector<int>> result;
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> level;
+            level.reserve(levelSize);
+
+            for (int i = 0; i < levelSize; i++) {
+                Node* curr = q.front();
+                q.pop();
+
+                level.push_back(curr->val);
+
+                for (auto child : curr->children) {
+                    if (child) q.push(child);
+                }
+            }
+
+            result.push_back(move(level));
+        }
+
+        return result;
+    }
+};
+
+// Accepted
