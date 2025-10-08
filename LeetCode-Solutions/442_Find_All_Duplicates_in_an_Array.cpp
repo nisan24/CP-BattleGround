fd3d5ace42873fbe1291e26a7cab73f2e7fe6bcf
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-08 22:11:58 +0600

    1 problem added

---
+++ LeetCode-Solutions/442_Find_All_Duplicates_in_an_Array.cpp
@@ -0,0 +1,30 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-08 22:11:33
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> duplicates;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int index = abs(nums[i]) - 1;
+
+            if (nums[index] < 0) {
+                duplicates.push_back(index + 1);
+            } 
+            else {
+                nums[index] = -nums[index];
+            }
+        }
+        return duplicates;
+    }
+};
+
+// Accepted
