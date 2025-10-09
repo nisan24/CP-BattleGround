e4e52ecc73e91f2223e99a1ded859b0c57277ba2
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-09 15:33:00 +0600

    1 problem added

---
+++ LeetCode-Solutions/451_Sort_Characters_By_Frequency.cpp
@@ -0,0 +1,37 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-09 15:32:40
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char, int> freq;
+        for (char c : s)
+            freq[c]++;
+
+        priority_queue<pair<int, char>> maxHeap;
+        for (auto &[ch, count] : freq)
+            maxHeap.push({count, ch});
+
+        string result;
+        while (!maxHeap.empty()) {
+            auto [count, ch] = maxHeap.top();
+            maxHeap.pop();
+            result.append(count, ch);
+        }
+
+        return result;
+    }
+};
+
+// Accepted
