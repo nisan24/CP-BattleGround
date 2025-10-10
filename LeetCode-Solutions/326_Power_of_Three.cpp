e977b550e24b1cafb04e6d197411b72e67c67c8d
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-10 22:28:31 +0600

    1 problem added

---
+++ LeetCode-Solutions/326_Power_of_Three.cpp
@@ -0,0 +1,22 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-10 22:27:59
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while(n % 3 == 0){
+            n /= 3;
+        }
+        return n == 1;
+    }
+};
+
+// Accepted
