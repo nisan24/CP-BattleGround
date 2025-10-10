09a0b6c1e8522f7a9d0c531772783af1fa3e02a2
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-10 22:28:50 +0600

    1 problem added

--- LeetCode-Solutions/326_Power_of_Three.cpp
+++ LeetCode-Solutions/326_Power_of_Three.cpp
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/power-of-three/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-10 22:27:59
