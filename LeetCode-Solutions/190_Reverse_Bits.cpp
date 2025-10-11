e78e6826db2279bc50f9d9e8a7e8e9bb893ab20c
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-11 23:02:03 +0600

    1 problem added

---
+++ LeetCode-Solutions/190_Reverse_Bits.cpp
@@ -0,0 +1,27 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-11 23:01:42
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+ 
+class Solution {
+public:
+    int reverseBits(int n) {
+        uint32_t ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+
+        return ans;
+    }
+};
+
+// Accepted
