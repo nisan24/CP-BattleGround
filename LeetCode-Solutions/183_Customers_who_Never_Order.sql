b642a3954983437d25767e06754838eb085bc01d
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-12 21:40:06 +0600

    1 problem added

--- LeetCode-Solutions/183_Customers_who_Never_Order.sql
+++ LeetCode-Solutions/183_Customers_who_Never_Order.sql
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/customers-who-never-order/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-12 21:39:12
