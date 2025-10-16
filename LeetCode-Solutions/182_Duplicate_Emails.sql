370b1497da11bf4ea272ae53e67ae43055884433
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-16 21:43:37 +0600

    1 problem added

--- LeetCode-Solutions/182_Duplicate_Emails.sql
+++ LeetCode-Solutions/182_Duplicate_Emails.sql
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/duplicate-emails/description/
- *  Problem Name :  
+ *  Problem Name : Duplicate Emails
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-16 21:42:38
  *  Language     : C++
