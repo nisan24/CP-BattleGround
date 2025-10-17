512adfa1284d04a6c279107724ea8a0370589c51
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-17 22:56:04 +0600

    1 problem solved

---
+++ LeetCode-Solutions/559_Maximum_Depth_of_N_ary_Tree.cpp
@@ -0,0 +1,46 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-17 22:55:26
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (!root) return 0;
+
+        int maxChildDepth = 0;
+        for (Node* child : root->children) {
+            maxChildDepth = max(maxChildDepth, maxDepth(child));
+        }
+
+        return maxChildDepth + 1;
+    }
+};
+
+// Accepted
