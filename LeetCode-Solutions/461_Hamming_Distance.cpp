552f6d721521447acaba3b1c6f7b4e8de490d428
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-18 22:38:08 +0600

    1 problem added

---
+++ LeetCode-Solutions/461_Hamming_Distance.cpp
@@ -0,0 +1,24 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-18 22:37:43
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int n = x ^ y, count = 0;
+        while (n) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+};
+
+// Accepted
