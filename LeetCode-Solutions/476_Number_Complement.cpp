d4d2830dbcdb79b4d3869ff2d8fbff911ca29238
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-19 22:10:56 +0600

    1 problem added

---
+++ LeetCode-Solutions/476_Number_Complement.cpp
@@ -0,0 +1,27 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-19 22:10:21
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int findComplement(int num) {
+        long long power = 1, result = 0;
+        while (num > 0) {
+            int bit = num % 2;
+            int flipped = (bit == 0) ? 1 : 0;
+            result += flipped * power;
+            power *= 2;
+            num /= 2;
+        }
+        return result;
+    }
+};
+
+// Accepted
