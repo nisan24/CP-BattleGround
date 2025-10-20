d2859f32dcb1d361f5ba5769c203d4c57511c7d8
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-20 23:29:28 +0600

    1 problem added

---
+++ LeetCode-Solutions/561_Array_Partition.cpp
@@ -0,0 +1,26 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-20 23:29:07
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i += 2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+};
+
+// Accepted
