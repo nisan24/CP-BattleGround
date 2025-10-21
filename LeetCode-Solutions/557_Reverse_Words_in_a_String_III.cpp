3dfdc9766e1c63ea197d096a7e96bb17ea31b9f6
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-21 23:07:30 +0600

    1 problem added

---
+++ LeetCode-Solutions/557_Reverse_Words_in_a_String_III.cpp
@@ -0,0 +1,28 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-21 23:07:12
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        string word, result;
+
+        bool first = true;
+        while (ss >> word) {
+            reverse(word.begin(), word.end());
+            if (!first) result += ' ';
+            result += word;
+            first = false;
+        }
+        return result;
+    }
+};
+
+// Accepted 
