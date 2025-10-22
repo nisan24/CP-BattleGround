a32cbff194a0eeb7be3c2e120e92f15f702793f5
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-22 22:42:01 +0600

    1 problem added

---
+++ LeetCode-Solutions/206_Reverse_Linked_List.cpp
@@ -0,0 +1,33 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-22 22:41:29
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return newHead;
+    }
+};
+
+// Accepted
