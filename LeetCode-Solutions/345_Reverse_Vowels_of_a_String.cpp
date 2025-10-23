80a798d0f18ee1622105e0768b009a0642ad42e6
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-23 13:22:56 +0600

    1 problem added

---
+++ LeetCode-Solutions/345_Reverse_Vowels_of_a_String.cpp
@@ -0,0 +1,31 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-23 13:22:33
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        const string vowels = "aeiouAEIOU";
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            while (left < right && vowels.find(s[left]) == string::npos)
+                left++;
+
+            while (left < right && vowels.find(s[right]) == string::npos)
+                right--;
+            
+            swap(s[left++], s[right--]);
+        }
+        return s;
+    }
+};
+
+// Accepted
