51657695ef9c117ea519b50c148dbc797ac4d0e1
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-24 22:06:00 +0600

    1 problem solved

---
+++ LeetCode-Solutions/197_Rising_Temperature.sql
@@ -0,0 +1,19 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-24 22:05:23
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+# Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+  ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
+
+# Accepted
