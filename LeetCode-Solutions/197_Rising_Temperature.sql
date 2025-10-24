857103eea219e14460bd1ef72a3b8baf602bbecf
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-24 22:06:27 +0600

    1 problem solved

--- LeetCode-Solutions/197_Rising_Temperature.sql
+++ LeetCode-Solutions/197_Rising_Temperature.sql
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/rising-temperature/description/
- *  Problem Name :  
+ *  Problem Name : Rising Temperature
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-24 22:05:23
  *  Language     : C++
