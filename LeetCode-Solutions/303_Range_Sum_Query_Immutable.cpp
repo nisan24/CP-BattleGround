8cf2503ca29b4b0bafb107ec1672783b851ca3af
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-26 19:40:14 +0600

    1 problem solved

---
+++ LeetCode-Solutions/303_Range_Sum_Query_Immutable.cpp
@@ -0,0 +1,37 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-26 19:39:53
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class NumArray {
+private:
+    vector<int> prefix;
+
+public:
+    NumArray(const vector<int>& nums) {
+        int n = nums.size();
+        prefix.resize(n + 1, 0);
+
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+// Accepted
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
