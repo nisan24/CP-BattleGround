a835753814e86c2543cdfd8ca0c026da4a68d276
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-27 23:12:51 +0600

    Updated

---
+++ LeetCode-Solutions/387_First_Unique_Character_in_a_String.cpp
@@ -0,0 +1,28 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-27 23:12:39
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int firstUniqChar(const string& s) {
+        vector<int> freq(26, 0); 
+
+        for (char c : s)
+            freq[c - 'a']++;
+        
+        for (int i = 0; i < s.size(); i++)
+            if (freq[s[i] - 'a'] == 1)
+                return i;
+        
+        return -1;
+    }
+};
+
+// Accepted
