0721cf990e8bec5c3b93a769c0aeba5dcfe145c0
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-27 23:13:14 +0600

    Updated

--- LeetCode-Solutions/387_First_Unique_Character_in_a_String.cpp
+++ LeetCode-Solutions/387_First_Unique_Character_in_a_String.cpp
@@ -1,13 +1,12 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
- *  Problem Name :  
+ *  Problem Link : https://leetcode.com/problems/first-unique-character-in-a-string/description/
+ *  Problem Name : First Unique Character in a String
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-27 23:12:39
  *  Language     : C++
  * -------------------------------------------------
  */
-
 
 class Solution {
 public:
