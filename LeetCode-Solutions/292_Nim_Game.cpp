4c81110e3e14ad6729d52f5dbeeef01ddbdf99a8
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-30 15:07:31 +0600

    1 problem added

--- LeetCode-Solutions/292_Nim_Game.cpp
+++ LeetCode-Solutions/292_Nim_Game.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/nim-game/description/
- *  Problem Name :  
+ *  Problem Name : 292. Nim Game
  *  Author       : Nisan Hossain
  * Created At   : 2025-10-30 15:06:47
  *  Language     : C++
