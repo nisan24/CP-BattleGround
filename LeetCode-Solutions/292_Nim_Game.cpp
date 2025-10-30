656a24f511bd144a50a861e98248e36b2e039e94
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-30 15:07:09 +0600

    1 problem added

---
+++ LeetCode-Solutions/292_Nim_Game.cpp
@@ -0,0 +1,19 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-30 15:06:47
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4;
+    }
+};
+
+// Accepted
