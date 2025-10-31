0c8d5d46451fd12a7a427979e5838c0b6c0fee26
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-10-31 20:39:28 +0600

    1 problem solved

---
+++ LeetCode-Solutions/350_Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,37 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-10-31 20:39:06
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        int i = 0, j = 0;
+        vector<int> ans;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Accepted
