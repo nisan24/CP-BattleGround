c9b726992a882764a20ae62b30f14a9efe08e1a6
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-01 23:39:16 +0600

    1 problem solved

---
+++ LeetCode-Solutions/495_Teemo_Attacking.cpp
@@ -0,0 +1,30 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-01 23:38:28
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        if (timeSeries.empty()) return 0;
+        
+        int total = 0;
+        int n = timeSeries.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            total += min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+
+        return total + duration;
+    }
+};
+
+
+
+// Accepted
