f6e11796d3a30b0d5231018586c3835dd6e10af1
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-02 20:38:10 +0600

    1 problem solved

---
+++ LeetCode-Solutions/455_Assign_Cookies.cpp
@@ -0,0 +1,33 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-02 20:37:50
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0, j = 0;
+        int count = 0;
+
+        while (i < g.size() && j < s.size()) {
+            if (g[i] <= s[j]) {
+                count++;
+                i++;
+            }
+            j++;
+        }
+
+        return count;
+    }
+};
+
+// Accepted
