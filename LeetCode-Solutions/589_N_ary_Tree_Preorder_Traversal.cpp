4ad21a040ea20de7e8c6f8bb839a732c24a1f698
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-04 21:42:06 +0600

    1 problem solved

--- LeetCode-Solutions/589_N_ary_Tree_Preorder_Traversal.cpp
+++ LeetCode-Solutions/589_N_ary_Tree_Preorder_Traversal.cpp
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/n-ary-tree-preorder-traversal/description/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-11-04 21:41:19
