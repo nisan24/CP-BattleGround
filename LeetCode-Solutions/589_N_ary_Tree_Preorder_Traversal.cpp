82bda4cbe5969f6b0c64dda7a5b1b4f5bb252f99
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-04 21:41:51 +0600

    1 problem solved

---
+++ LeetCode-Solutions/589_N_ary_Tree_Preorder_Traversal.cpp
@@ -0,0 +1,52 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-04 21:41:19
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+private:
+    void dfs(Node* root, vector<int>& result) {
+        if (!root) return;
+        result.push_back(root->val);
+
+        for (Node* child : root->children) {
+            dfs(child, result);
+        }
+    }
+
+public:
+    vector<int> preorder(Node* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+};
+
+// Accepted
