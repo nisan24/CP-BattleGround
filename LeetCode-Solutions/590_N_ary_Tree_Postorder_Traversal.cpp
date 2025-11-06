41730258f42166b7ed797bbb36d9538f30384bf2
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-06 20:09:40 +0600

    1 problem solved

---
+++ LeetCode-Solutions/590_N_ary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,52 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-06 20:09:16
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+private:
+    void dfs(Node* root, vector<int>& result) {
+        if (!root)
+            return;
+
+        for (Node* child : root->children) {
+            dfs(child, result);
+        }
+        result.push_back(root->val);
+    }
+
+public:
+    vector<int> postorder(Node* root) {
+        vector<int> result;
+        dfs(root, result);
+        return result;
+    }
+};
+
+// Accepted
