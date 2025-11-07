a2f55a80c4ae87fcabb84226b9463a88c374fd1b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-07 22:09:57 +0600

    1 problem solved

---
+++ LeetCode-Solutions/598_Range_Addition_II.cpp
@@ -0,0 +1,29 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-07 22:09:33
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    int maxCount(int m, int n, vector<vector<int>>& ops) {
+        if (ops.empty()) 
+            return m * n;
+
+        int minRow = m, minCol = n;
+
+        for (const auto& op : ops) {
+            minRow = min(minRow, op[0]);
+            minCol = min(minCol, op[1]);
+        }
+
+        return minRow * minCol;
+    }
+};
+
+// Accepted
