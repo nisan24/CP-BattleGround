9c84233f9b181bdd0c6cfc966a536b45cd0ee3ee
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-08 23:16:08 +0600

    1 problem solved

---
+++ LeetCode-Solutions/401_Binary_Watch.cpp
@@ -0,0 +1,31 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-08 23:15:36
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+class Solution {
+public:
+    vector<string> readBinaryWatch(int turnedOn) {
+        vector<string> result;
+
+        for (int hour = 0; hour < 12; ++hour) {
+            for (int minute = 0; minute < 60; ++minute) {
+                if (__builtin_popcount(hour) + __builtin_popcount(minute) ==
+                    turnedOn) {
+                    result.push_back(to_string(hour) + ":" +
+                                     (minute < 10 ? "0" : "") +
+                                     to_string(minute));
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Accepted
