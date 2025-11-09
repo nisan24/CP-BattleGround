639323a035ce943ff13c12f60d27f63817bd2825
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-09 16:32:25 +0600

    Updated

--- LeetCode-Solutions/401_Binary_Watch.cpp
+++ LeetCode-Solutions/401_Binary_Watch.cpp
@@ -13,8 +13,8 @@ public:
     vector<string> readBinaryWatch(int turnedOn) {
         vector<string> result;
 
-        for (int hour = 0; hour < 12; ++hour) {
-            for (int minute = 0; minute < 60; ++minute) {
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
                 if (__builtin_popcount(hour) + __builtin_popcount(minute) ==
                     turnedOn) {
                     result.push_back(to_string(hour) + ":" +
