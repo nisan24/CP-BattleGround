9457c663ce78587691039c500871a0cba41f7591
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-12 23:08:35 +0600

    1 problem solved

---
+++ LeetCode-Solutions/383_Ransom_Note.cpp
@@ -0,0 +1,34 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-12 23:08:13
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int> freq(26, 0);
+
+        for (char ch : magazine)
+            freq[ch - 'a']++;
+
+        for (char ch : ransomNote) {
+            if (--freq[ch - 'a'] < 0)
+                return false;
+        }
+
+        return true;
+    }
+};
+
+// Accepted
