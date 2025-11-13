5dc34a8958c2159fd7781abe5ed6ca6efdfccadf
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-13 23:02:43 +0600

    1 problem solved

---
+++ LeetCode-Solutions/541_Reverse_String_II.cpp
@@ -0,0 +1,31 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-13 23:02:21
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.size();
+        
+        for (int i = 0; i < n; i += 2 * k) {
+            int end = min(i + k, n);
+            reverse(s.begin() + i, s.begin() + end);
+        }
+
+        return s;
+    }
+};
+
+// Accepted
