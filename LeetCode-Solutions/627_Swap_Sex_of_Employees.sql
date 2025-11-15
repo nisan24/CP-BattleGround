770e0f477d43052cd9119efb3c6c6490a79259fa
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-15 16:36:34 +0600

    1 problem solved

--- LeetCode-Solutions/627_Swap_Sex_of_Employees.sql
+++ LeetCode-Solutions/627_Swap_Sex_of_Employees.sql
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/swap-sex-of-employees/description/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-11-15 16:35:38
