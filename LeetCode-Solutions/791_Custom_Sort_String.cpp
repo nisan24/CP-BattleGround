c550e980d5e07eb17d88cade518d2b52dea24c64
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-18 18:58:16 +0600

    1 problem solved

---
+++ LeetCode-Solutions/791_Custom_Sort_String.cpp
@@ -0,0 +1,38 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-18 18:57:56
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        string result;
+
+        for (char c : order) {
+            if (freq.count(c)) {
+                result.append(freq[c], c);
+                freq.erase(c);
+            }
+        }
+
+        for (auto &p : freq) {
+            result.append(p.second, p.first);
+        }
+
+        return result;
+    }
+};
+
+// Accepted
