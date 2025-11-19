3d15e2ea133947083be0974febff7a0a2cb7ce8b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-19 23:34:14 +0600

    1 problem added

---
+++ LeetCode-Solutions/169_Majority_Element.cpp
@@ -0,0 +1,31 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-19 23:33:50
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = 0, count = 0;
+        
+        for (int num : nums) {
+            if (count == 0) 
+                candidate = num;
+
+            // if (num == candidate) count++;
+            // else count--;
+            count += (num == candidate) ? 1 : -1;
+        }
+        
+        return candidate;
+    }
+};
+
+// Accepted
