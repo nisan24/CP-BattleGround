2587ee006b1412ec3a14343886734b8e75164021
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-20 22:23:15 +0600

    1 problem added

---
+++ LeetCode-Solutions/257_Binary_Tree_Paths.cpp
@@ -0,0 +1,49 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-20 22:20:56
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, vector<string>& ans, string path) {
+        if (!root)
+            return;
+
+        path += to_string(root->val);
+
+        if (!root->left && !root->right) {
+            ans.push_back(path);
+            return;
+        }
+
+        dfs(root->left, ans, path + "->");
+        dfs(root->right, ans, path + "->");
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        dfs(root, ans, "");
+        return ans;
+    }
+};
+
+// Accepted
