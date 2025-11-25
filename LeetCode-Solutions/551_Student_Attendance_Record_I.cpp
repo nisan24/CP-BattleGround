fdf0b717577ff64e2f304839c63958ff3af566dd
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-25 21:58:50 +0600

    1 problem solved

---
+++ LeetCode-Solutions/551_Student_Attendance_Record_I.cpp
@@ -0,0 +1,38 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-25 21:58:18
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    bool checkRecord(string s) {
+        int A = 0;
+        int L = 0;
+
+        for (char c : s) {
+            if (c == 'A') {
+                A++;
+                if (A == 2) return false;
+                L = 0;
+            }
+            else if (c == 'L') {
+                L++;
+                if (L == 3) return false;
+            }
+            else {
+                L = 0;
+            }
+        }
+
+        return true;
+    }
+};
+
+// Accepted
