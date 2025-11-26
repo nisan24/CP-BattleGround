6075fd4b5cf8facebbb7a5c77e80b0cfc18fb656
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-26 21:26:38 +0600

    1 problem solved

---
+++ LeetCode-Solutions/594_Longest_Harmonious_Subsequence.cpp
@@ -0,0 +1,30 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-26 21:26:06
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        
+        for (int x : nums) freq[x]++;
+
+        int longest = 0;
+
+        for (auto& [num, cnt] : freq) {
+            if (freq.count(num + 1)) {
+                longest = max(longest, cnt + freq[num + 1]);
+            }
+        }
+
+        return longest;
+    }
+};
+
+
+// Accepted
