51179ee5bd127712e589ee044756b5994af848c3
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-11-29 22:42:44 +0600

    1 problem solved

---
+++ LeetCode-Solutions/342_Power_of_Four.cpp
@@ -0,0 +1,26 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-11-29 22:42:05
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if (n <= 0)
+            return false;
+
+        if (n & (n - 1))
+            return false;
+
+        return (n - 1) % 3 == 0;
+    }
+};
+
+// Accepted
