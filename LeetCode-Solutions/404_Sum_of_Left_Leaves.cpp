d97ef9020942bec12aa93cc94d237b22fe4b0f6b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-01 21:29:30 +0600

    1 problem

---
+++ LeetCode-Solutions/404_Sum_of_Left_Leaves.cpp
@@ -0,0 +1,40 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-01 21:29:04
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int sum = 0;
+
+        if (root->left && !root->left->left && !root->left->right) {
+            sum += root->left->val;
+        }
+
+        return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+    }
+};
+
+// Accepted
