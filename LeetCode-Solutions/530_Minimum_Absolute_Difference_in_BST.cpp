d5a2625de5248e4290e62c8e0bd61847b46ad9f6
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-04 22:39:20 +0600

    Problem solved

--- LeetCode-Solutions/530_Minimum_Absolute_Difference_in_BST.cpp
+++ LeetCode-Solutions/530_Minimum_Absolute_Difference_in_BST.cpp
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/minimum-absolute-difference-in-bst/description/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2025-12-04 22:38:34
