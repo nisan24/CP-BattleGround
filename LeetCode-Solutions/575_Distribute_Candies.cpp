73e91a3f10d2f1354a79d1dbf5b3dace7e2c02cb
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-05 23:17:04 +0600

    1 problem solved

---
+++ LeetCode-Solutions/575_Distribute_Candies.cpp
@@ -0,0 +1,21 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-05 23:15:22
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_set<int> uniqueTypes(candyType.begin(), candyType.end());
+        return min((int)uniqueTypes.size(), (int)candyType.size() / 2);
+    }
+};
+
+// Accepted
