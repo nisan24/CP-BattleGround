4bfcca4988dbd3cbc691a3aceb9952841fd6d347
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-06 23:38:14 +0600

    1 problem solved

---
+++ LeetCode-Solutions/586_Customer_Placing_the_Largest_Number_of_Orders.sql
@@ -0,0 +1,21 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-06 23:37:52
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+# Write your MySQL query statement below
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
+
+
+# Accepted
