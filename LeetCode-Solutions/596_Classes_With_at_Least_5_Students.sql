257e79bba3eedfce57943f870558b895610047cc
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-09 22:07:00 +0600

    1 problem added

--- LeetCode-Solutions/596_Classes_With_at_Least_5_Students.sql
+++ LeetCode-Solutions/596_Classes_With_at_Least_5_Students.sql
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/classes-with-at-least-5-students/description/
- *  Problem Name :  
+ *  Problem Name : 596. Classes With at Least 5 Students
  *  Author       : Nisan Hossain
  * Created At   : 2025-12-09 22:05:58
  *  Language     : C++
