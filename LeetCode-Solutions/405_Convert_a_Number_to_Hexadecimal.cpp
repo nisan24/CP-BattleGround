9a34e7c1905d47137f05b06fa2332c30d90cd349
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-10 21:33:18 +0600

    1 problem added

---
+++ LeetCode-Solutions/405_Convert_a_Number_to_Hexadecimal.cpp
@@ -0,0 +1,32 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-10 21:32:40
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+
+        const string hex = "0123456789abcdef";
+        string ans;
+        unsigned int n = num;
+
+        for (int i = 0; i < 8 && n > 0; i++) {
+            ans.push_back(hex[n & 15]);
+            n >>= 4;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+// Accepted
