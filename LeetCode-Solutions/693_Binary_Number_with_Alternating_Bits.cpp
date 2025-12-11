1873393dd12abb44b68ad8b0f973253cb4682c7e
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-11 23:26:42 +0600

    Update

---
+++ LeetCode-Solutions/693_Binary_Number_with_Alternating_Bits.cpp
@@ -0,0 +1,21 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : https://leetcode.com/problems/binary-number-with-alternating-bits/description/
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-11 23:25:26
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        long long x = (long long)n ^ (long long)(n >> 1);
+        return (x & (x + 1)) == 0;
+    }
+};
+
+// Accepted
