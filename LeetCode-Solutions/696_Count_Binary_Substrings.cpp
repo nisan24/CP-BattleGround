e99be074abb58bc08bc12b994bfd19b7bd097fb0
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-16 23:12:01 +0600

    696 no problem solved

---
+++ LeetCode-Solutions/696_Count_Binary_Substrings.cpp
@@ -0,0 +1,36 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-16 23:11:32
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int prevCount = 0;
+        int currCount = 1;
+        int result = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[i - 1]) {
+                currCount++;
+            } else {
+                result += min(prevCount, currCount);
+                prevCount = currCount;
+                currCount = 1;
+            }
+        }
+
+        result += min(prevCount, currCount);
+
+        return result;
+    }
+};
+
+// Accepted
