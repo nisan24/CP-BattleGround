b4addf396cb072c238b25d359441258a2b7fc9ec
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-21 20:31:17 +0600

    762 no problem solved

---
+++ LeetCode-Solutions/762_Prime_Number_of_Set_Bits_in_Binary.cpp
@@ -0,0 +1,30 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-21 20:29:55
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        static const unordered_set<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
+        
+        int count = 0;
+
+        for (int i = left; i <= right; i++) {
+            int bits = __builtin_popcount(i);
+            if (primes.count(bits)) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+// Accepted
