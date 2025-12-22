0310b82db24c8844f21883305b66df2f32cd1015
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-22 21:19:16 +0600

    617 no problem solved

---
+++ LeetCode-Solutions/617_Merge_Two_Binary_Trees.cpp
@@ -0,0 +1,42 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-22 21:18:13
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (!t1)
+            return t2;
+        if (!t2)
+            return t1;
+
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+
+        return t1;
+    }
+};
+
+// Accepted
