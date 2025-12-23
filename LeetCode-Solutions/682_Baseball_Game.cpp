283a55581885a488a7e280e48ce48f9e7c9d3d37
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-23 22:57:13 +0600

    682 no problem solved

---
+++ LeetCode-Solutions/682_Baseball_Game.cpp
@@ -0,0 +1,41 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-23 22:55:40
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        stack<int> st;
+
+        for (auto& s : ops) {
+
+            if (s == "C") {
+                st.pop();
+            } else if (s == "D") {
+                st.push(st.top() * 2);
+            } else if (s == "+") {
+                int a = st.top();
+                st.pop();
+                int b = st.top();
+                st.push(a);
+                st.push(a + b);
+            } else {
+                st.push(stoi(s));
+            }
+        }
+
+        int total = 0;
+        while (!st.empty()) {
+            total += st.top();
+            st.pop();
+        }
+        return total;
+    }
+};
+
+// Accepted
