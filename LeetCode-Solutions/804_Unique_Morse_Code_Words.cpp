463650dcce38ecd05f1bc73cf20dc8975c425971
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-24 23:36:32 +0600

    804 no problem solved

---
+++ LeetCode-Solutions/804_Unique_Morse_Code_Words.cpp
@@ -0,0 +1,44 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-24 23:33:02
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    int uniqueMorseRepresentations(vector<string>& words) {
+        // Small letter character er jonno Morse code er map/array
+        const string morse_code[] = {
+            ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---",
+            "-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-",
+            "..-","...-",".--","-..-","-.--","--.."
+        };
+
+        // Unique transformation gulo rakhar jonno set
+        unordered_set<string> result_set;
+
+        // Proti ti word er jonno loop
+        for (const string& word : words) {
+            string current_morse = "";
+            
+            // Word er character gulo ke morse code e convert korar loop
+            for (char c : word) {
+                current_morse += morse_code[c - 'a'];
+            }
+            
+            // Set e insert (Duplicate thakle set auto bad diye dibe)
+            result_set.insert(current_morse);
+        }
+
+        // Set er size e hobe unique result
+        return result_set.size();
+    }
+};
+
+// Accepted
