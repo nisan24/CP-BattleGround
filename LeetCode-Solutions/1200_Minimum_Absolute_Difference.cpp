f296ba1e92098038e4bdebbfff8b83d0c93a830d
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-12-27 21:13:39 +0600

    1200 no problem solved

---
+++ LeetCode-Solutions/1200_Minimum_Absolute_Difference.cpp
@@ -0,0 +1,38 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2025-12-27 21:12:51
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int minDiff = INT_MAX;
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            int diff = arr[i + 1] - arr[i];
+
+            if (diff < minDiff) {
+                minDiff = diff;
+                ans = {{arr[i], arr[i + 1]}};
+            } 
+            else if (diff == minDiff) {
+                ans.push_back({arr[i], arr[i + 1]});
+            }
+        }
+
+        return ans;
+    }
+};
+
+// Accepted
