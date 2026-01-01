4228ca87c7cb1c78e0a5073a88b756b02a51480b
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-01 22:36:17 +0600

    59 no problem solved

---
+++ LeetCode-Solutions/59_Spiral_Matrix_II.cpp
@@ -0,0 +1,58 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-01 22:35:57
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+
+        vector<vector<int>> v(n, vector<int>(n));
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int val = 1;
+
+        while (top <= bottom && left <= right) {
+
+            // left → right
+            for (int i = left; i <= right; i++)
+                v[top][i] = val++;
+
+            // top moves down
+            top++;
+
+            // top → bottom
+            for (int i = top; i <= bottom; i++)
+                v[i][right] = val++;
+
+            // right moves left
+            right--;
+
+            if (top <= bottom) {
+                // right → left
+                for (int i = right; i >= left; i--)
+                    v[bottom][i] = val++;
+                bottom--;
+            }
+
+            if (left <= right) {
+                // bottom → top
+                for (int i = bottom; i >= top; i--)
+                    v[i][left] = val++;
+                left++;
+            }
+        }
+
+        return v;
+    }
+};
+
+// Accepted
