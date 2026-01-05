f7dcccdaf0be62b24f6c917c2d424138dc5114e5
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-05 22:41:23 +0600

    1207 no problem solved

---
+++ LeetCode-Solutions/1207_Unique_Number_of_Occurrences.cpp
@@ -0,0 +1,31 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-05 22:40:56
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freq;
+
+        for (int x : arr)
+            freq[x]++;
+
+        unordered_set<int> seen;
+        for (auto& p : freq) {
+            if (!seen.insert(p.second).second)
+                return false;
+        }
+
+        return true;
+    }
+};
+
+// Accepted
