e424687ee71f81184a5dc147e695aa07ccab104a
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-06 21:33:51 +0600

    942 no problem solved

---
+++ LeetCode-Solutions/942_DI_String_Match.cpp
@@ -0,0 +1,32 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-06 21:33:30
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int low = 0, high = s.size();
+        vector<int> ans;
+        ans.reserve(s.size() + 1);
+
+        for (char c : s) {
+            if (c == 'I')
+                ans.push_back(low++);
+            else
+                ans.push_back(high--);
+        }
+        ans.push_back(low);
+
+        return ans;
+    }
+};
+
+// Accepted
