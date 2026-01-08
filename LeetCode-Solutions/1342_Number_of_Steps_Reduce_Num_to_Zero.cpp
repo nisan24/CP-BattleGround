f15f3fa19d50592aad58c45a426138768c831973
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-08 21:41:51 +0600

    1342 no problem solved

--- LeetCode-Solutions/1342_Number_of_Steps_Reduce_Num_to_Zero.cpp
+++ LeetCode-Solutions/1342_Number_of_Steps_Reduce_Num_to_Zero.cpp
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/description/
- *  Problem Name :  
+ *  Problem Name : 1342. Number of Steps to Reduce a Number to Zero
  *  Author       : Nisan Hossain
  * Created At   : 2026-01-08 21:40:36
  *  Language     : C++
