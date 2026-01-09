95d53f36ffa20438c4990c27fcc4a607abae653e
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-09 21:40:03 +0600

    1021 no problem solved

---
+++ LeetCode-Solutions/1021_Remove_Outermost_Parentheses.cpp
@@ -0,0 +1,34 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-09 21:39:36
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string res;
+        int depth = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                if (depth > 0)
+                    res.push_back(c);
+                depth++;
+            } else { // ')'
+                depth--;
+                if (depth > 0)
+                    res.push_back(c);
+            }
+        }
+        return res;
+    }
+};
+
+// Accepted 
