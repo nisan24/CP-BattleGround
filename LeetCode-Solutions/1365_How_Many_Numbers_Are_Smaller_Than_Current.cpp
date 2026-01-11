65a0699524e7b9e74c1e5e4e4b246faaf400b288
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-11 21:56:41 +0600

    1365 no problem solved

---
+++ LeetCode-Solutions/1365_How_Many_Numbers_Are_Smaller_Than_Current.cpp
@@ -0,0 +1,37 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-11 21:56:13
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> freq(101, 0);
+
+        // Count frequency
+        for (int x : nums)
+            freq[x]++;
+
+        // Prefix sum
+        for (int i = 1; i <= 100; i++)
+            freq[i] += freq[i - 1];
+
+        // Build answer
+        vector<int> ans;
+        ans.reserve(nums.size());
+
+        for (int x : nums)
+            ans.push_back(x == 0 ? 0 : freq[x - 1]);
+
+        return ans;
+    }
+};
+
+// Accepted
