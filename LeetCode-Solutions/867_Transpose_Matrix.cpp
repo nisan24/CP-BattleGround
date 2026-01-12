04947267029d526d2ebcd4e836cddbfb08cf288f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-12 23:05:05 +0600

    867 no problem solved

--- LeetCode-Solutions/867_Transpose_Matrix.cpp
+++ LeetCode-Solutions/867_Transpose_Matrix.cpp
@@ -1,6 +1,6 @@
 /*
  * -------------------------------------------------
- *  Problem Link : 
+ *  Problem Link : https://leetcode.com/problems/transpose-matrix/
  *  Problem Name :  
  *  Author       : Nisan Hossain
  * Created At   : 2026-01-12 23:04:31
