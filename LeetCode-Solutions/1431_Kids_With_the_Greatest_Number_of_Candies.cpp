ae24ca86bb7e900e9dc966b76dd27b470b47f653
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-13 21:44:46 +0600

    1431 no problem solved

--- LeetCode-Solutions/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
+++ LeetCode-Solutions/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -7,7 +7,6 @@
  *  Language     : C++
  * -------------------------------------------------
  */
-
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
