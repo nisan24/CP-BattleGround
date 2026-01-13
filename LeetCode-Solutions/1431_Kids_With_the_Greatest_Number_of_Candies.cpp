6995d5823cea5caecda3ef20a6b54ff8748bc984
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-13 21:44:01 +0600

    1431 no problem solved

---
+++ LeetCode-Solutions/1431_Kids_With_the_Greatest_Number_of_Candies.cpp
@@ -0,0 +1,27 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-13 21:43:31
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int mx = *max_element(candies.begin(), candies.end());
+        vector<bool> res;
+        res.reserve(candies.size());
+
+        for (int c : candies)
+            res.push_back(c + extraCandies >= mx);
+
+        return res;
+    }
+};
+
+// Accepted
