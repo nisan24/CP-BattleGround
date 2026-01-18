92a6d5d19dcd22b771bd82edbecf5e2c4fb1a238
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-18 23:01:00 +0600

    1683 no problem solved

---
+++ LeetCode-Solutions/1683_Invalid_Tweets.cpp
@@ -0,0 +1,19 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-18 22:59:58
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+# Write your MySQL query statement below
+
+SELECT tweet_id
+FROM Tweets
+WHERE CHAR_LENGTH(content) > 15;
+
+# Accepted
