7569d05715c07a3c6ca95e130d8f3689fc624915
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-19 16:41:57 +0600

    1587 no problem solved

---
+++ LeetCode-Solutions/1587_Bank_Account_Summary_II.cpp
@@ -0,0 +1,23 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-19 16:41:25
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+# Write your MySQL query statement below
+
+SELECT u.name, SUM(t.amount) AS balance
+FROM Users u
+JOIN Transactions t
+    ON u.account = t.account
+GROUP BY u.account, u.name
+HAVING balance > 10000;
+
+
+# Accepted
