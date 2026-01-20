c93d67fca0215e07f81fd63195698aff446dc5f3
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-20 20:48:55 +0600

    1470 no problem solved

---
+++ LeetCode-Solutions/1470_Shuffle_the_Array.cpp
@@ -0,0 +1,27 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name :  
+ *  Author       : Nisan Hossain
+ * Created At   : 2026-01-20 20:48:26
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        res.reserve(2 * n);
+
+        for (int i = 0; i < n; i++) {
+            res.push_back(nums[i]);
+            res.push_back(nums[i + n]);
+        }
+        return res;
+    }
+};
+
+// Accepted
