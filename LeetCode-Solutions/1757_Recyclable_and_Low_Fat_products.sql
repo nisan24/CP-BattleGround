65a994a84621f870a085592e77928b1f781da44f
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-24 19:56:53 +0600

    1757 no problem solved

--- LeetCode-Solutions/1757_Recyclable_and_Low_Fat_products.sql
+++ LeetCode-Solutions/1757_Recyclable_and_Low_Fat_products.sql
@@ -1,7 +1,7 @@
 /*
  * -------------------------------------------------
  *  Problem Link : https://leetcode.com/problems/recyclable-and-low-fat-products/
- *  Problem Name : 
+ *  Problem Name : 1757. Recyclable and Low Fat Products
  *  Author       : Nisan Hossain
  *  Created At   : 2026-01-24 19:55:47
  *  Language     : C++
