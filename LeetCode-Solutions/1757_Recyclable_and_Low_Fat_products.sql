c41dbb5ba9ca575f61e212720d6471e4d6eda41d
Md. Nisan Hossain <nisanhossain76@gmail.com>
2026-01-24 19:56:34 +0600

    1757 no problem solved

---
+++ LeetCode-Solutions/1757_Recyclable_and_Low_Fat_products.sql
@@ -0,0 +1,16 @@
+/*
+ * -------------------------------------------------
+ *  Problem Link : 
+ *  Problem Name : 
+ *  Author       : Nisan Hossain
+ *  Created At   : 2026-01-24 19:55:47
+ *  Language     : C++
+ * -------------------------------------------------
+ */
+
+
+# Write your MySQL query statement below
+SELECT product_id FROM
+Products WHERE low_fats='Y' AND recyclable='Y';
+
+# Accepted
